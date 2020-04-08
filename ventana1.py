60992d54b187c0620fde50a9f4adb70e8df2c2b3
dav22 <63010256+dav22@users.noreply.github.com>
2020-04-08 20:56:16 -0300

    Add files via upload

--- ventana1.py
+++ ventana1.py
@@ -16,7 +16,7 @@ ventana.title('eduPython')
 
 btnSalir = Button(ventana, text='Salir',command=ventana.destroy).pack(side=BOTTOM)
 
-Asignaturas = ttk.Combobox(ventana, values=["Aritmetica", "Programacion Basica","Geometria","Trigonometria","Algebra"])
+Asignaturas = ttk.Combobox(ventana, values=["Aritmetica", "Programacion Basica","Geometria","Trigonometria","CoronaVirus"])
 
 lbl = Label(ventana,text="Lista de Asignaturas",bg="#483D8B",fg="white",font=("Arial", 20))
 
@@ -31,6 +31,39 @@ btnSelec.pack()
 Asignaturas.current(1)
 
 ventana.mainloop()
+
+def funcionPruebaTemas():
+    messagebox.showinfo(message=Temas.get(), title="Tema Seleccionado")
+
+ventanaDos = Tk()
+
+ventanaDos.geometry('400x300')
+
+ventanaDos.configure(bg = '#483D8B')
+
+ventanaDos.title('eduPython')
+
+btnSalir = Button(ventanaDos, text='Salir',command=ventanaDos.destroy).pack(side=BOTTOM)
+
+Temas = ttk.Combobox(ventanaDos, values=["Sumas", "Python","Triangulos","Teorema de Pitágoras","Medidas de prevención"])
+
+lbl = Label(ventanaDos,text="Lista de temas",bg="#483D8B",fg="white",font=("Arial", 20))
+
+btnSelec = Button(ventanaDos,text="Seleccionar",command=funcionPruebaTemas)
+
+lbl.pack(side=TOP)
+
+Temas.pack()
+
+btnSelec.pack()
+
+Temas.current(1)
+
+ventanaDos.mainloop()
+
+
+
+
 
 
 
